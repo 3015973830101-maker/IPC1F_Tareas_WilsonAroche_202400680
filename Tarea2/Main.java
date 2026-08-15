bbbd37c8928f457c680f5bf81b5380ddfb5dc437
3015973830101-maker <3015973830101@ingenieria.usac.edu.gt>
2026-08-14 18:20:38 -0600

    Agregar tipos de datos y variables

--- Tarea2/Main.java
+++ Tarea2/Main.java
@@ -1,3 +1,17 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        // Variables con diferentes tipos de datos
+        int edad = 20;
+        double promedio = 85.5;
+        char seccion = 'F';
+        boolean activo = true;
+
+        System.out.println("===== DATOS DEL ESTUDIANTE =====");
+        System.out.println("Edad: " + edad);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Seccion: " + seccion);
+        System.out.println("Activo: " + activo);
+    }
 }
